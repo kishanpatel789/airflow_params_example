92bb3dd3232155f8188f3d764237050ec06a72c3
Kishan Patel <mail@kishanpatel.dev>
2024-10-10 11:24:00 -0500

    Add docstrings

--- airflow/dags/src/python_runner.py
+++ airflow/dags/src/python_runner.py
@@ -1,10 +1,22 @@
 import logging
+from airflow.models import TaskInstance
 
 logger = logging.getLogger(__name__)
 STANDARD_PACKAGES = ["boto3>=1.35.0", "pandas>=2.2.0"]
 
 
-def process_parameters(python_file_path: str, extra_packages: list, kw_args: dict, ti):
+def process_parameters(python_file_path: str, extra_packages: list, kw_args: dict, ti: TaskInstance):
+    """Processes parameters passed into the DAG run configuration. Stores finalized packages as an Xcom.
+
+    Args:
+        python_file_path (str): AWS S3 path to target python file. Must begin with 's3://' and end with '.py'
+        extra_packages (list): List of python packages (with optional versions) to use when running python file
+        kw_args (dict): Keyword arguments to be passed to python file
+        ti (TaskInstance): Airflow task instance associated with task run
+
+    Raises:
+        ValueError: if python_file_path is an invalid S3 path
+    """
 
     # validate python_file_path
     if not python_file_path.startswith("s3://"):
@@ -33,7 +45,17 @@ def process_parameters(python_file_path: str, extra_packages: list, kw_args: dic
     ti.xcom_push(key="final_packages_str", value=final_packages_str)
 
 
-def run_python_file(python_file_path, final_packages, file_kw_args):
+def run_python_file(python_file_path: str, final_packages: list, file_kw_args: dict):
+    """Downloads target python file from S3, parses content, and executes file.
+
+    Args:
+        python_file_path (str): AWS S3 path to target python file. 
+        final_packages (list): List of python packages used when running python file
+        file_kw_args (dict): Keyword arguments to be passed to python file at run time
+
+    Raises:
+        SyntaxError: if python file content is invalid
+    """
 
     import logging
     import importlib
@@ -52,23 +74,22 @@ def run_python_file(python_file_path, final_packages, file_kw_args):
         logger.addHandler(handler)
 
     # print target and actual packages installed
-    logger.info(f"final_packages {final_packages}")
+    logger.info(f"final_packages: {final_packages}")
     packages = sorted(
         [f"{p.name}=={p.version}" for p in importlib.metadata.distributions()]
     )
     logger.info("Here are the packages currently installed: ")
     logger.info("    " + "\n    ".join(packages))
 
-    # get python file content - s3://bucket-name/somewhere/file/path.py
+    # parse s3 path for target python file
     parsed_url = urlparse(python_file_path)
     bucket_name = parsed_url.netloc
     object_key = parsed_url.path.lstrip("/")
     logger.info(f"Attempting to use file '{object_key}' in bucket '{bucket_name}'")
 
+    # download python file content
     python_file_path = Path.cwd() / "python_file_to_run.py"
     logger.info(f"Local python file path: {python_file_path}")
-
-    # download python file content
     try:
         s3_client = boto3.client("s3")
         with open(python_file_path, "w+b") as f:
